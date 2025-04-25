5be0f7996459577b3ca6bfce17cff7a956b87762
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-04-25 16:07:13 +0200

    componenete fichaBirra hecho

--- src/views/Usuarios.jsx
+++ src/views/Usuarios.jsx
@@ -1,23 +1,14 @@
 import NuevoPedido from "../components/NuevoPedido"
+import FichaBirra from "../components/FichaBirra"
 export default function Usuarios(){
     console.log("Usuarios cargado")
     return(
         <div className="container mt-3 p-5 border shadow-lg ">
       <h1 class="text-center mb-5 ">----- Vista usuario -----</h1>
         <NuevoPedido/>
+        <FichaBirra/>
         
-        <div class="col-6 border ">
-          <div class="p-3 d-flex">
-              <img src="estrella.jpg" alt="" className="w-100" />
-              <img src="estrella.jpg" alt="" class="w-100" />
-            </div>
-            <div>
-              <h4 class="">Estrella Galicia</h4>
-              <p>Cerveza suave y equilibrada con un sabor ligeramente amargo y aroma a malta.</p>
-            </div>
-            
-  
-          </div>
+        
         </div>
             
     );
