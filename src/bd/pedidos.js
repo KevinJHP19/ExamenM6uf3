5fcafde383fd92a5b1fef567f08ae2fd13a5ddc0
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-04-25 16:16:24 +0200

    crear array de comandas

---
+++ src/bd/pedidos.js
@@ -0,0 +1,25 @@
+// Array per emmagatzemar les comandes
+export const pedidos = [
+    {
+        numeroMesa: 1,
+        grupo: "Amigos",
+        cerveza: "Estrella Galicia",
+        cantidad: 3,
+        estado: "pendiente" // Valor per defecte
+    },
+    {
+        numeroMesa: 2,
+        grupo: "Família",
+        cerveza: "Heinken",
+        cantidad: 5,
+        estado: "pendiente" // Valor per defecte
+    },
+    {
+        numeroMesa: 3,
+        grupo: "Companys de feina",
+        cerveza: "Stout",
+        cantidad: 2,
+        estado: "pendiente"
+    }
+];
+
